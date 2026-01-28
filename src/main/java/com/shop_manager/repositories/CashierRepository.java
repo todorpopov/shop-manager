0fa9177557833c23cff372f17e2b5d157d48fca3
todorpopov <todorpopov02@gmail.com>
2026-01-29 00:51:00 +0200

    Add repository

---
+++ src/main/java/com/shop_manager/repositories/CashierRepository.java
@@ -0,0 +1,42 @@
+package com.shop_manager.repositories;
+
+import com.shop_manager.models.Cashier;
+import com.shop_manager.storage_engine.InMemoryTable;
+import com.shop_manager.storage_engine.InMemoryDatabase;
+
+import java.util.List;
+
+public class CashierRepository {
+    private static CashierRepository instance;
+
+    private final InMemoryTable<Cashier> cashiers = InMemoryDatabase.getInstance().cashiers();
+
+    private CashierRepository() {}
+
+    public static CashierRepository getInstance(){
+        if(instance == null){
+            instance = new CashierRepository();
+        }
+        return instance;
+    }
+
+    public Cashier getCashierById(Long id) {
+        return cashiers.get(id);
+    }
+
+    public List<Cashier> getAllCashiers() {
+        return cashiers.all();
+    }
+
+    public void addCashier(Cashier cashier) {
+        cashiers.insert(cashier);
+    }
+
+    public void updateCashier(Cashier cashier) {
+        cashiers.update(cashier);
+    }
+
+    public void deleteCashier(long id) {
+        cashiers.delete(id);
+    }
+}
